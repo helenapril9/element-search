237345ea1784dee5ca918c2d9f690807a8229c7f
Nelly Porvatova <dolgikhhel@yandex.ru>
2023-04-14 10:02:34 +0400

    second commit

--- menu/task.js
+++ menu/task.js
@@ -1,15 +1,19 @@
-// Получаем список всех элементов меню
-const menuItems = document.querySelectorAll('.menu__item');
-// Добавляем обработчик события "наведения курсора мыши" на каждый элемент меню
-for (item of menuItems)
- menuItems.forEach(function(item) {
-     const link = item.querySelector('.menu__link');
-     const subMenu = item.querySelector('.menu_sub');       
-// Показываем подменю при наведении курсора мыши на элемент  
-   link.addEventListener('mouseover', function() {
-   subMenu.classList.add('menu_active');   
-   });
-   item.addEventListener('mouseleave', function() {
-     subMenu.classList.remove('menu_active');
-   });    
- });    
+const links = Array.from(document.querySelectorAll('.menu__link'));
+links.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    const parent = link.closest('.menu__item');       
+    const submenu = parent.querySelector('.menu');    
+    if (submenu) {
+      event.preventDefault();
+      submenu.classList.toggle('menu_active');
+      if (submenu.classList.contains('menu_active')) {
+        const otherSubmenus = Array.from(document.querySelectorAll('.menu_active'));
+        otherSubmenus.forEach((otherSubmenu) => {
+          if (otherSubmenu !== submenu) {
+          otherSubmenu.classList.remove('menu_active');
+          }
+        });
+    }
+    }
+  });
+});
